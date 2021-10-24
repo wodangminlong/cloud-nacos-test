eacb871d67f40319f5be20301deaea9ab97d445f
dml <15191681021@163.com>
2021-10-24 18:19:53 +0800

    add mybatis plus gateway test

--- mybatis-plus-gateway-openfeign/mybatis-plus-gateway-server/src/test/java/com/ml/MybatisPlusGatewayServerApplicationTests.java
+++ mybatis-plus-gateway-openfeign/mybatis-plus-gateway-server/src/test/java/com/ml/MybatisPlusGatewayServerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ml;
 
+import com.ml.controller.TestController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class MybatisPlusGatewayServerApplicationTests {
 
     @Test
     void contextLoads() {
+        TestController testController = new TestController();
+        System.out.println(testController.test());
     }
 
 }
