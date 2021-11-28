51d8f18458b989e2b8f28ebca63529be6b48bc8f
dml <15191681021@163.com>
2021-11-28 22:39:57 +0800

    add mybatis-plus seata

--- mybatis-plus-gateway-openfeign/mybatis-plus-provider-server/src/main/java/com/ml/MybatisPlusProviderServerApplication.java
+++ mybatis-plus-gateway-openfeign/mybatis-plus-provider-server/src/main/java/com/ml/MybatisPlusProviderServerApplication.java
@@ -3,13 +3,14 @@ package com.ml;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Administrator
  * @date 2021/10/24 00:26
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @MapperScan("com.ml.mapper")
 public class MybatisPlusProviderServerApplication {
